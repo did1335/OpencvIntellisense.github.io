f21376008411835eca500d724826895dd4f01c3d
朱庭宏 <did1335@gmail.com>
2019-10-30 11:16:47 +0800

    Update copyright.js

--- opencv_extension2/js/view/copyright.js
+++ opencv_extension2/js/view/copyright.js
@@ -2,7 +2,7 @@ Vue.component('copyright', {
     template: '<p v-html="msg"></p>',
     data: function () {
       return {
-        msg: 'copyright &#169; 2019 CVIntelli Community'
+        msg: 'copyright &#169; 2019 CVIntelli Taichung Community'
       }
     }
 });
